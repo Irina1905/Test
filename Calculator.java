5183b5da7fb1c229d7e20c7c6c943460abb39907
home <svoya_1995@mail.ru>
2023-07-31 15:13:46 +0300

    test

--- Calculator.java
+++ Calculator.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Введите операцию: ");
-        String operation = scanner.nextLine();
+        String input = scanner.nextLine();
+        System.out.println(calc(input));
+    }
 
-        String[] parts = operation.split(" ");
+    public static String calc(String input) throws Exception {
+        String[] parts = input.split(" ");
         int operand1 = Integer.parseInt(parts[0]);
         String operator = parts[1];
         int operand2 = Integer.parseInt(parts[2]);
         int result = 0;
-        if (operand1 > 0 && operand1 <= 10 && operand2 > 0 && operand2 <= 10 && parts.length == 3) {
+        if (operand1 > 0 && operand1 <= 10 && operand2 > 0
+                && operand2 <= 10 && parts.length == 3) {
             switch (operator) {
                 case "+" -> result = operand1 + operand2;
                 case "-" -> result = operand1 - operand2;
@@ -24,5 +27,6 @@ public class Calculator {
         } else {
             throw new Exception();
         }
+        return String.valueOf(result);
     }
 }
