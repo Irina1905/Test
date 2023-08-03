d318f08cd0fa06f0402f9f6f4fca676257fa2ce2
home <svoya_1995@mail.ru>
2023-08-03 13:26:48 +0300

    test

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
     public static String calc(String input) {
         String[] values = input.split(" ");
         if (values.length != 3) {
-            throw new IllegalArgumentException("Invalid input");
+            throw new IllegalArgumentException("Недопустимый ввод");
         }
 
         int a;
@@ -20,20 +20,21 @@ public class Calculator {
             a = Integer.parseInt(values[0]);
             b = Integer.parseInt(values[2]);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Invalid numbers");
+            throw new IllegalArgumentException("Недопустимые числа");
+        }
+        if (a < 1 || a > 10) {
+            throw new IllegalArgumentException("throws Exception //Числа должны быть от 1 до 10");
+        }
+        if (b < 1 || b > 10) {
+            throw new IllegalArgumentException("throws Exception //Числа должны быть от 1 до 10");
         }
 
         int result = switch (values[1]) {
             case "+" -> a + b;
             case "-" -> a - b;
             case "*" -> a * b;
-            case "/" -> {
-                if (b == 0) {
-                    throw new IllegalArgumentException("Cannot divide by zero");
-                }
-                yield a / b;
-            }
-            default -> throw new IllegalArgumentException("Invalid operation");
+            case "/" -> a / b;
+            default -> throw new IllegalArgumentException("Недопустимая операция");
         };
         return String.valueOf(result);
     }
